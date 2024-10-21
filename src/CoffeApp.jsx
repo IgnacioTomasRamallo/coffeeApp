8c679e3911ee49b3277ebf33e4523454cbc1248d
Ignacio <ignacioramalloherrera@gmail.com>
2024-10-20 23:34:50 -0300

    push

--- src/CoffeApp.jsx
+++ src/CoffeApp.jsx
@@ -4,13 +4,18 @@ import { Cards } from "./components/cards/Cards";
 
 function App() {
   const [state, setState] = useState();
-
+    //tareas a realizar para el 21
   return (
     <div className="bg-[#111315] flex w-full justify-center sm:py-8 py-5">
       <div className="bg-image"></div>
       <div className="bg-[#1B1D1F] p-10 sm:flex sm:flex-col sm:items-center w-3/5 rounded-lg shadow-xl">
         <div className="text-center sm:w-2/6 ">
           <h2 className="p-3 text-[#FEF7EE] ">Our Collection</h2>
+          <p className="text-information text-wrap text-[#6F757C]">
+            Introducing our Coffee Collection, a selection of unique coffees
+            from different roast types and origins, expertly roasted in small
+            batches and shipped fresh weekly.
+          </p>
           <p className="text-information text-wrap text-[#6F757C]">
             Introducing our Coffee Collection, a selection of unique coffees
             from different roast types and origins, expertly roasted in small
